0832eb401de1f331f0a22229158c82fb327367a8
hue2Two <h.faisal3399@gmail.com>
2023-11-15 21:06:46 -0500

    player input on game board

--- main.py
+++ main.py
@@ -22,4 +22,17 @@ def printBoard(board):
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
 
-printBoard(board)
+#take player input
+def playerInput(board):
+    inp = int(input("enter a number 1-9: ")) #input
+
+    #check if input is valid
+    if inp >= 1 and inp <= 9 and board[inp-1] == "_":
+        board[inp - 1] = currentPlayer
+    else:
+        print("oops player is already in that spot")
+
+#game loop
+while gameRunning:
+    printBoard(board) #print the game board
+    playerInput(board) #check the player input
