c0a2d9cc20d96b11d3d7f75f3e92c1d53b655c73
hue2Two <h.faisal3399@gmail.com>
2023-11-19 10:25:59 -0500

    minimax implemented

--- TicTacToe_MinMax.py
+++ TicTacToe_MinMax.py
@@ -1,11 +1,9 @@
-board = {
-    1: ' ', 2: ' ', 3: ' ',
-    4: ' ', 5: ' ', 6: ' ',
-    7: ' ', 8: ' ', 9: ' '
-}
-
-player = '0'
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+player = 'O'
 computer = 'X'
+
 
 def printBoard(board):
     print(board[1] + "|" + board[2] + "|" + board[3])
@@ -15,20 +13,17 @@ def printBoard(board):
     print(board[7] + "|" + board[8] + "|" + board[9])
     print("\n")
 
-# printBoard(board)
 
-#if there is a space to go to
 def spaceIsFree(position):
     if board[position] == ' ':
         return True
     return False
 
-#inserting a letter at a point in the board
+
 def insertLetter(letter, position):
     if spaceIsFree(position):
         board[position] = letter
         printBoard(board)
-
         if checkDraw():
             print("Draw!")
             exit()
@@ -41,10 +36,11 @@ def insertLetter(letter, position):
                 exit()
         return
     else:
-        print("invalid position")
-        position = int(input("please enter a new position: "))
-        insertLetter(letter, position) #recursive
+        print("Invalid position")
+        position = int(input("Please enter a new position: "))
+        insertLetter(letter, position)
         return
+
 
 def checkWin():
     if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
@@ -66,21 +62,85 @@ def checkWin():
     else:
         return False
 
+
+def checkWhichMarkWon(mark):
+    if (board[1] == board[2] and board[1] == board[3] and board[1] == mark):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+        return True
+    else:
+        return False
+
+
 def checkDraw():
     for key in board.keys():
         if board[key] == ' ':
             return False
-        return True
+    return True
+
 
 def playerMove():
-    position = int(input("enter a postion for 0"))
+    position = int(input("Enter a position for 'O': "))
     insertLetter(player, position)
     return
 
+
 def compMove():
-    position = int(input("enter a postion for X"))
-    insertLetter(computer, position)
+    bestScore = -800
+    bestMove = 0
+    for key in board.keys():
+        if board[key] == ' ':
+            board[key] = computer
+            score = minimax(board, False)
+            board[key] = ' '
+            if score > bestScore:
+                bestScore = score
+                bestMove = key
+    insertLetter(computer, bestMove)
     return
+
+
+def minimax(board, isMaximizing):
+    if checkWhichMarkWon(computer):
+        return 1
+    elif checkWhichMarkWon(player):
+        return -1
+    elif checkDraw():
+        return 0
+
+    if isMaximizing:
+        bestScore = -800
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = computer
+                score = minimax(board, False)
+                board[key] = ' '
+                if score > bestScore:
+                    bestScore = score
+        return bestScore
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = player
+                score = minimax(board, True)
+                board[key] = ' '
+                if score < bestScore:
+                    bestScore = score
+        return bestScore
+
 
 while not checkWin():
     compMove()
